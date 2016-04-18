b3c5d46d66f9a6cff47765ed3be3ced9aa2d1d5f
George Ayris <george.ayris@huddle.com>
2016-04-18 21:49:12 +0100

    Calculate english frequency scoref

--- src/set1.rb
+++ src/set1.rb
@@ -22,3 +22,91 @@ def fixed_xor(hex_string_1, hex_string_2)
   binary_2 = hex_to_binary(hex_string_2)
   binary_1.zip(binary_2).map {|x,y| binary_to_hex(x^y) }.join
 end
+
+def xor_against_single_character(hex_string, character)
+  fixed_xor(hex_string, character*hex_string.length)
+end
+
+def calculate_letter_frequencies(input_string)
+  def letter?(character)
+    character =~ /[[:alpha:]]/
+  end
+
+  frequency = Hash.new(0)
+  input_string.split("").each do |x|
+    x.downcase!
+    if letter?(x)
+      frequency[x] += 1
+    end
+  end
+
+  frequency
+end
+
+def calculate_english_frequency_score(frequency_hash)
+  def most_frequent(hash)
+    frequency = 0
+    letter = nil
+    hash.each { |key, value|
+      if value > frequency
+        letter = key
+        frequency = value
+      end
+    }
+    [letter, frequency]
+  end
+
+  def least_frequent(hash)
+    frequency = nil
+    letter = nil
+    hash.each { |key, value|
+      if frequency.nil? or value < frequency
+        letter = key
+        frequency = value
+      end
+    }
+    [letter, frequency]
+  end
+
+  most_frequent_english_letters = "etaoin"
+  least_frequent_english_letters = "vkjxqz"
+
+  least_frequent_letters = Hash.new
+  most_frequent_letters = Hash.new
+  frequency_hash.each do |key, value|
+    if least_frequent_letters.size < 6
+      least_frequent_letters[key] = value
+    else
+      letter, frequency = most_frequent(least_frequent_letters)
+      if value < frequency
+        least_frequent_letters.delete(letter)
+        least_frequent_letters[key] = value
+      end
+    end
+
+    if most_frequent_letters.size < 6
+      most_frequent_letters[key] = value
+    else
+      letter, frequency = least_frequent(most_frequent_letters)
+      if value > frequency
+        most_frequent_letters.delete(letter)
+        most_frequent_letters[key] = value
+      end
+    end
+  end
+
+  frequency_score = 0
+  most_frequent_english_letters.split("").each do |x|
+    if most_frequent_letters.key?(x)
+      frequency_score += 1
+    end
+  end
+
+  least_frequent_english_letters.split("").each do |x|
+    if least_frequent_letters.key?(x)
+      frequency_score += 1
+    end
+  end
+
+  frequency_score
+end
--- test/set1.rb
+++ test/set1.rb
@@ -7,14 +7,38 @@ class Set1Tests < MiniTest::Test
   end
 
   def test_challenge_1
-    assert_equal hex_to_base64(@input), "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+    assert_equal "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t",
+                 hex_to_base64(@input)
   end
 
-  def test_challenge_2_simple
-    assert_equal fixed_xor("1c", "68"), "74"
+  def test_fixed_xor
+    assert_equal "74", fixed_xor("1c", "68")
   end
 
   def test_challenge_2
-    assert_equal fixed_xor("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965"), "746865206b696420646f6e277420706c6179"
+    assert_equal "746865206b696420646f6e277420706c6179",
+                 fixed_xor("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965")
+  end
+
+  def test_letter_frequencies
+    frequency_hash = calculate_letter_frequencies("Aaaahhh, letters")
+    assert_equal 4, frequency_hash['a']
+    assert_equal 3, frequency_hash['h']
+    assert_equal 1, frequency_hash['l']
+    assert_equal 2, frequency_hash['e']
+    assert_equal 2, frequency_hash['t']
+    assert_equal 1, frequency_hash['r']
+    assert_equal 1, frequency_hash['s']
+  end
+
+  def test_calculate_frequency_score
+    frequency_hash = calculate_letter_frequencies("etaoin etaoin vkjxqz")
+    assert_equal 12, calculate_english_frequency_score(frequency_hash)
+  end
+
+  def test_single_byte_xor_cypher
+    decoded_text, key = single_byte_xor_cypher("f29bdad69be8cbdac9cfdad8cec8")
+    assert_equal "I am Spartacus", decoded_text
+    assert_equal 'b', key
   end
 end
