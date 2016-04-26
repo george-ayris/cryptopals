274d4c028e01d90906c19aafdbf799e37f78fafc
George Ayris <george.ayris@huddle.com>
2016-04-26 23:06:38 +0100

    Massively simplified the scoring algorithm

--- src/english_score_calculator.rb
+++ src/english_score_calculator.rb
@@ -1,5 +1,3 @@
-require_relative 'frequency_hash.rb'
-
 class EnglishScoreCalculator
   def initialize(potential_english_string)
     @potential_english_string = potential_english_string
@@ -15,42 +13,15 @@ class EnglishScoreCalculator
   end
 
   def is_more_likely_to_be_english_than(other_calculator)
-    @english_score <= other_calculator.score()
+    @english_score > other_calculator.score()
   end
 
   private
   def calculate_english_score(potential_english_string)
-    return 1000 unless potential_english_string.ascii_only?
+    return 0 unless potential_english_string.ascii_only?
 
-    english_letter_frequencies = { 'a' => 0.08167, 'b' => 0.01492, 'c' => 0.02782,
-      'd' => 0.04253, 'e' => 0.12702, 'f' => 0.0228, 'g' => 0.02015, 'h' => 0.06094,
-      'i' => 0.06966, 'j' => 0.00153, 'k' => 0.00722, 'l' => 0.04025, 'm' => 0.02406,
-      'n' => 0.06749, 'o' => 0.07507, 'p' => 0.01929, 'q' => 0.00095, 'r' => 0.05987,
-      's' => 0.06327, 't' => 0.09056, 'u' => 0.02758, 'v' => 0.00978, 'w' => 0.02361,
-      'x' => 0.00015, 'y' => 0.01974, 'z' => 0.00074 }
-
-    frequencies = FrequencyHash.new(potential_english_string)
-
-    return 500 unless frequencies.a_to_z_entries?
-
-    chi_squared = 0
-    english_letter_divisor = 1
-    [*('a'..'z')].each do |x|
-      english_letter_divisor *= 2 unless frequencies[x] == 0
-      chi_squared += ((frequencies[x] - english_letter_frequencies[x])**2 / english_letter_frequencies[x])
-    end
-
-    # The following should be refactored to use the frequency hash
-    # Punish multiple spaces in a row
-    potential_english_string.scan(/  /).each { |x| english_letter_divisor/2 }
-
-    invalid_punctuation = ['#','\\','@','{','}','[',']','`','|','_','^','*']
-    potential_english_string.split('').each do |x|
-      if invalid_punctuation.include? x or !x.ascii_only?
-        english_letter_divisor = english_letter_divisor/2
-      end
-    end
-
-    chi_squared/english_letter_divisor
+    number_of_common_letters = potential_english_string.scan(/[ETAOIN SHRDLU]/i).length
+    return number_of_common_letters
   end
+
 end
--- src/repeating_key_xor_encoder.rb
+++ src/repeating_key_xor_encoder.rb
@@ -3,7 +3,7 @@ require_relative 'bytes.rb'
 class RepeatingKeyXorEncoder
   def initialize(key, plain_text)
     key = Bytes.new(key.bytes)
-    plain_text = Bytes.new(plain_text.bytes) 
+    plain_text = Bytes.new(plain_text.bytes)
 
     @cypher_text = repeating_key_encode(key, plain_text)
   end
@@ -18,7 +18,7 @@ class RepeatingKeyXorEncoder
     number_of_bytes = plain_text.number_of_bytes()
     key_length = key.number_of_bytes()
 
-    [*(0..(number_of_bytes-1))].each do |i|
+    (0..(number_of_bytes-1)).each do |i|
       key_byte = key.byte_n(i % key_length)
       plain_text_byte = plain_text.byte_n(i)
       cypher_text = cypher_text.concat(plain_text_byte.xor_with_byte(key_byte))
